9cf0c2d75a3b41bf46007fe894b08001b9509864
CSimpson <cdsimpsonnavy2010@yahoo.com>
2024-01-23 15:42:08 -0500

    initial commit

---
+++ src/Interfaces/FileAble.java
@@ -0,0 +1,35 @@
+package Interfaces;
+
+import java.io.IOException;
+
+public interface FileAble {
+
+    /**
+     * importObjects takes a filepath from parameters or input by the user and loads data
+     * from lines of a .csv into a data structure.
+     */
+    void importObjects(String filePath) throws IOException;
+
+    /**
+     * exportObjects creates a .csv file from a list of objects and their associated fields.
+     * Assume the method throws an IOException due to the use of fileReader or BufferedReader.
+     * @return a message informing the user if the file was created.
+     */
+    String exportObjects(String filePath, String header) throws IOException;
+    //create string to return
+    //create the csv file
+    //create the string filepath, specifying a directory path
+    //concat the csv file string to filepath
+    //using file writer object with the filepath input
+    //write the headers separated by commas on line 1
+    //for each object in the list,
+    //write each line with the variables in order separated by commas
+    //after loop runs, close the file writer.
+    //update success message informing user file was created.
+    //if any error with filename or filepath occurs,
+    //throw an IO exception informing the user of the error
+    //return the success message
+
+    //adds a file type suffix to a filename, derived from the filepath or passed manually.
+    String addFileType(String fileName);
+}
---
+++ src/Interfaces/ProbabilityOperations.java
@@ -0,0 +1,38 @@
+package Interfaces;
+
+/**
+ * The ProbabilityOperations interface is a generics interface that should be used
+ * when constructing classes that use or model some probability calculations. This
+ * could be the end goal of the class (calculating probability using a specific
+ * formula or theorem) or serve the operations of a class or program.
+ */
+public interface ProbabilityOperations<E> {
+
+    //a method to count the number of unique objects in a list.
+    E countUniqueObjects(E aList);
+
+    //a method to assign simple probabilities to the unique objects in a list.
+    E assignObjectProbabilities(E aList);
+
+    //methods to create unions and intersections of objects based on their supertypes.
+
+    //unite is useful if an object has multiple data structures or lists of objects.
+    E unite(E theLists);
+    //intersect is useful when considering comparable objects.
+    E intersect(E theLists);
+
+    //methods to check lists of objects against the axioms of probability.
+    boolean checkAxioms(E aList);
+
+    //methods to return strings informing the user of the probabilities of objects in a list.
+    String listProbabilities(E aList);
+
+    //method to calculate specific probabilities for the objects within a data structure.
+    double calculateProbability(E aList);
+
+    //methods to calculate central tendency for a list of objects
+    E calcMean(E aList);
+    E calcMode(E aList);
+    E calcMedian(E aList);
+    E calcStandardDev(E aList);
+}
