9e0b30c447be5aefb980c521195141a1f131f658
link409c <cdsimpsonnavy2010@yahoo.com>
2024-01-30 13:59:44 -0500

    moved to test folder

---
+++ src/MiniProjects/PokemonCardHands/TesterAndResults/PokemonCardHandsTester.java
@@ -0,0 +1,11 @@
+package MiniProjects.PokemonCardHands.TesterAndResults;
+
+import MiniProjects.PokemonCardHands.Program.PokemonHands;
+
+public class PokemonCardHandsTester {
+
+    public static void main(String[] args){
+        PokemonHands pkmn = new PokemonHands();
+
+    }
+}
