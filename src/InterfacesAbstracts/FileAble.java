f568db51884310a0e38b767dbf20fc0b4324b264
link409c <cdsimpsonnavy2010@yahoo.com>
2024-01-31 22:07:07 -0500

    changed comments and javadoc

--- src/InterfacesAbstracts/FileAble.java
+++ src/InterfacesAbstracts/FileAble.java
@@ -6,7 +6,7 @@ public interface FileAble {
 
     /**
      * importObjects takes a filepath from parameters or input by the user and loads data
-     * from lines of a .csv into a data structure.
+     * from lines of a file into a data structure.
      */
     void importObjects(String filePath) throws IOException;
 
@@ -16,20 +16,10 @@ public interface FileAble {
      * @return a message informing the user if the file was created.
      */
     String exportObjects(String filePath) throws IOException;
-    //create string to return
-    //create the csv file
-    //create the string filepath, specifying a directory path
-    //concat the csv file string to filepath
-    //using file writer object with the filepath input
-    //write the headers separated by commas on line 1
-    //for each object in the list,
-    //write each line with the variables in order separated by commas
-    //after loop runs, close the file writer.
-    //update success message informing user file was created.
-    //if any error with filename or filepath occurs,
-    //throw an IO exception informing the user of the error
-    //return the success message
 
     //adds a file type suffix to a filename, derived from the filepath or passed manually.
     String addFileType(String fileName);
+
+    //add some unique identifier to the fileName to help user identify exported files
+    String addIdentifier(String fileName);
 }
