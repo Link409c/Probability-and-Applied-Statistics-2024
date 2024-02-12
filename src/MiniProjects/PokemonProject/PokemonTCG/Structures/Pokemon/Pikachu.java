d0d1efd64c622fa6fc55aec45083525c33477c01
link409c <cdsimpsonnavy2010@yahoo.com>
2024-02-12 11:56:17 -0500

    programmed pikachu class, a subclass of pokemon.

---
+++ src/MiniProjects/PokemonProject/PokemonTCG/Structures/Pokemon/Pikachu.java
@@ -0,0 +1,33 @@
+package MiniProjects.PokemonProject.PokemonTCG.Structures.Pokemon;
+
+import InterfacesAbstracts.Attackable;
+import MiniProjects.PokemonProject.PokemonTCG.Structures.MainSuperClasses.Pokemon;
+
+public class Pikachu extends Pokemon implements Attackable<Pokemon> {
+
+    public Pikachu(){
+        super();
+    }
+
+    /**
+     * Pikachu's First Attack is Tackle.
+     * @param theTarget the target of the attack
+     * @return the updated target object
+     */
+    @Override
+    public Pokemon attackOne(Pokemon theTarget) {
+        theTarget.setHp(theTarget.getHp() - 20);
+        return theTarget;
+    }
+
+    /**
+     * Pikachu's second attack is Electro Ball.
+     * @param theTarget the target of the attack
+     * @return the updated target object
+     */
+    @Override
+    public Pokemon attackTwo(Pokemon theTarget) {
+        theTarget.setHp(theTarget.getHp() - 40);
+        return theTarget;
+    }
+}
