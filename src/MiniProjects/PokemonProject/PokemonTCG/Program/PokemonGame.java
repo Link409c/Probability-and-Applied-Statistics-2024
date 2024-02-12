9fe8e944d58c18400b7f9a8f56bca987c4a646f2
link409c <cdsimpsonnavy2010@yahoo.com>
2024-02-12 12:10:02 -0500

    added interface method signatures

--- src/MiniProjects/PokemonProject/PokemonTCG/Program/PokemonGame.java
+++ src/MiniProjects/PokemonProject/PokemonTCG/Program/PokemonGame.java
@@ -5,10 +5,84 @@ import InterfacesAbstracts.FileAble;
 import MiniProjects.PokemonProject.PokemonTCG.Structures.MainSuperClasses.PokemonCard;
 import MiniProjects.PokemonProject.PokemonTCG.Structures.PokemonPlayer;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class PokemonGame implements CardGameEnvironment<PokemonCard, PokemonPlayer>, FileAble {
 
-    public PokemonGame(){
-        setPlayerOne();
+    @Override
+    public PokemonPlayer runGame() {
+        return null;
+    }
+
+    @Override
+    public void buildGame() {
+        //create both player objects
+        //create
+    }
+
+    @Override
+    public void conductTurn() {
+
+    }
+
+    @Override
+    public String printWholeField() {
+        return null;
+    }
+
+    @Override
+    public String inspectPlayerOneField() {
+        return null;
+    }
+
+    @Override
+    public String inspectPlayerTwoField() {
+        return null;
+    }
+
+    @Override
+    public String inspectPlayerOneStatus() {
+        return null;
+    }
+
+    @Override
+    public String inspectPlayerTwoStatus() {
+        return null;
+    }
+
+    @Override
+    public ArrayList<PokemonCard> inspectPlayerOneDiscard() {
+        return null;
+    }
+
+    @Override
+    public ArrayList<PokemonCard> inspectPlayerTwoDiscard() {
+        return null;
+    }
+
+    @Override
+    public void importObjects(String filePath) throws IOException {
+
+    }
+
+    @Override
+    public String exportObjects(String filePath) throws IOException {
+        return null;
+    }
+
+    @Override
+    public String addFileType(String fileName) {
+        return null;
+    }
+
+    @Override
+    public String addIdentifier(String fileName) {
+        return null;
+    }
+    public PokemonGame() {
+        setPlayerOne(new PokemonPlayer());
+        setPlayerTwo(new PokemonPlayer());
     }
 
     public PokemonPlayer getPlayerOne() {
