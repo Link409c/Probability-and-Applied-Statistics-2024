83dc634e4e7ebdab7cdc4f5eac3c3afc1433ed28
link409c <cdsimpsonnavy2010@yahoo.com>
2024-02-29 14:46:09 -0500

    data structure extending hand result for the rare candy hands program.

---
+++ src/MiniProjects/PokemonProject/PokemonCardHands/Structures/CandyHandResult.java
@@ -0,0 +1,21 @@
+package MiniProjects.PokemonProject.PokemonCardHands.Structures;
+
+public class CandyHandResult extends HandResult{
+
+    public CandyHandResult(int numPokemon, double successRate, int numCandy){
+        super(numPokemon, successRate);
+        setNumCandy(numCandy);
+    }
+    public int getNumCandy() {
+        return numCandy;
+    }
+
+    public void setNumCandy(int numCandy) {
+        this.numCandy = numCandy;
+    }
+
+    /**
+     * the total number of "Rare Candy" cards in the deck.
+     */
+    private int numCandy;
+}
