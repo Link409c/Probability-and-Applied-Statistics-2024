784f96325e3c564836c3caa264051935ed4e21a1
link409c <cdsimpsonnavy2010@yahoo.com>
2024-02-29 19:40:04 -0500

    Added evaluation methods. added deal prizes method.

--- src/MiniProjects/PokemonProject/PokemonCardHands/Program/RareCandyHands.java
+++ src/MiniProjects/PokemonProject/PokemonCardHands/Program/RareCandyHands.java
@@ -16,15 +16,24 @@ public class RareCandyHands extends PokemonHands{
 
     @Override
     public void runTrials(int numTrials) {
-
+        //run a number of trials for 1, 2, 3, 4 rare candy
     }
 
-    @Override
-    public boolean evaluateHand() {
+    public boolean evaluatePrizes(boolean handEvaluationResult){
+        //if the hand does not have a rare candy
+            //if the prizes have a rare candy
+                //return true
         return false;
     }
 
     @Override
+    public boolean evaluateHand() {
+        //if the hand contains a pokemon,
+            //if the hand contains a rare candy,
+                //return true
+        return false;
+    }
+
     public void makeDeck(int pokemonCount, int energyCount, int candyCount) {
         //make a new Deck Object
         Stack<PokemonCard> newDeck = new Stack<>();
@@ -51,5 +60,45 @@ public class RareCandyHands extends PokemonHands{
         setDeck(newDeck);
     }
 
+    /**
+     * After making the Deck data structure, populates the Prizes
+     * array with six cards drawn from the top of the deck.
+     */
+    public void dealPrizes(){
+        //shuffle the deck
+        shuffleDeck();
+        //get the deck
+        Stack<PokemonCard> theDeck = getDeck();
+        //pop 6 cards for prizes
+        int count = getPrizeCount();
+        PokemonCard[] prizes = getPrizes();
+        for(int i = 0; i < count; i++){
+            prizes[i] = theDeck.pop();
+        }
+        //set the prizes
+        setPrizes(prizes);
+        //set the deck
+        setDeck(theDeck);
+    }
+
+    public RareCandyHands(){
+        super();
+        setPrizes(new PokemonCard[getPrizeCount()]);
+    }
+
+    public PokemonCard[] getPrizes() {
+        return prizes;
+    }
+
+    public void setPrizes(PokemonCard[] prizes) {
+        this.prizes = prizes;
+    }
+
+    public int getPrizeCount(){
+        return PRIZE_COUNT;
+    }
+
     private PokemonCard[] prizes;
+
+    private static final int PRIZE_COUNT = 6;
 }
