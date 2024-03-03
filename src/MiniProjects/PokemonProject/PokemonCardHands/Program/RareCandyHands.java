969cd84e212b840603e640a7679ad59695e9e190
CSimpson <cdsimpsonnavy2010@yahoo.com>
2024-03-03 17:26:40 -0500

    implemented count method. finished run methods. tested program.

--- src/MiniProjects/PokemonProject/PokemonCardHands/Program/RareCandyHands.java
+++ src/MiniProjects/PokemonProject/PokemonCardHands/Program/RareCandyHands.java
@@ -5,58 +5,72 @@ import MiniProjects.PokemonProject.PokemonTCG.Structures.Items.RareCandy;
 import MiniProjects.PokemonProject.PokemonTCG.Structures.MainSuperClasses.Energy;
 import MiniProjects.PokemonProject.PokemonTCG.Structures.MainSuperClasses.Pokemon;
 import MiniProjects.PokemonProject.PokemonTCG.Structures.MainSuperClasses.PokemonCard;
-import StatsLibrary.StatsLibrary;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Stack;
 
 public class RareCandyHands extends PokemonHands{
 
+    /**
+     * runs the appropriate number of trials and writes the results to a file.
+     * @param filePath the directory to save the output file.
+     * @param numTrials the intended number of trials to run for each ratio
+     *                  of rare candy objects in the deck.
+     * @return calls the exportObjects method, which will produce a status message
+     * for the user.
+     * @throws IOException if the filepath is invalid.
+     */
     @Override
     public String runProgram(String filePath, int numTrials) throws IOException {
-        return "";
+        //run the appropriate amount of trials
+        runTrials(numTrials);
+        //export the results
+        filePath = filePath.concat("\\RareCandyHandsTest");
+        filePath = addIdentifier(filePath);
+        filePath = addFileType(filePath);
+        return exportObjects(filePath);
     }
 
-    public void runTrials(int numTrials, StatsLibrary statalyzer) {
-        //run a number of trials for 1, 2, 3, 4 rare candy
+    /**
+     * Run a number of trials to calculate the chances of success and bricking.
+     * Success is defined as seeing a hand which contains a Pokemon and a Rare Candy.
+     * Bricking is defined as having no Rare Candy in the hand, and seeing one in the Prizes.
+     * @param numTrials the amount of hands to draw for each ratio of Rare Candy in the deck.
+     */
+    @Override
+    public void runTrials(int numTrials) {
         //rare candy count
-        int candyCount = 0;
-        //initial probability of success
+        int candyCount = count(getDeck(), new RareCandy());
+        //probability of success (rare candy and pokemon in hand)
         double successRate;
+        //probability of brick (rare candy in prizes)
         double brickRate;
         //tracking successes in each trial run
         int successes;
-        //tracking candy location in each trial run
+        //tracking bricks in each trial run
         int bricks;
         //tracking hand result for each run
         boolean handResult;
-        //get accurate rare candy count in deck
-        if(getDeck() != null) {
-            for (PokemonCard c : getDeck()) {
-                if (c.getClass() == RareCandy.class) {
-                    candyCount++;
-                }
-            }
-        }
-        //run the program for
+        //run the program for up to 4 copies of rare candy
         while(candyCount < 5) {
             shuffleDeck();
             successes = bricks = 0;
             //run a number of trials to get hand results
             for(int i = 0; i < numTrials; i++){
-                //draw a hand
+                //deal the prizes
+                dealPrizes();
+                //then draw a hand
                 drawHand(getHandSize());
                 //evaluate
                 handResult = evaluateHand();
                 //if hand has no rare candy,
                 if(!handResult){
-                    //if no rare candy in prizes,
-                    if(!evaluatePrizes(false)){
-                        //neither event occurs.
-                        continue;
-                    }else{
-                        //else increment bricks
+                    //if rare candy in prizes,
+                    if(evaluatePrizes(false)) {
+                        //increment bricks
                         bricks++;
                     }
                 }else{
@@ -75,27 +89,28 @@ public class RareCandyHands extends PokemonHands{
             }
             //calculate the chance of success
             successRate = (double) successes / numTrials;
+            //calculate the chance of a brick
             brickRate = (double) bricks / numTrials;
-            getTheResults().add(new CandyHandResult(candyCount, successRate,
+            //get accurate pokemon count
+            int pokemonCount = count(getDeck(), new Pokemon());
+            //add to results
+            getCandyResults().add(new CandyHandResult(pokemonCount, successRate,
                     candyCount, brickRate));
-            /*
-            System.out.println(getTheResults().getLast().getNumPokemon() + ", "
-                    + getTheResults().getLast().getSuccessRate());
-            */
+            //after a set of trials, increment candy count
             candyCount++;
-            int pokemonCount = 0, energyCount = 0;
-            for(PokemonCard c : getDeck()){
-                if(c.getClass() == Pokemon.class){
-                    pokemonCount++;
-                }
-                else if(c.getClass() == Energy.class){
-                    energyCount++;
-                }
-            }
+            //get accurate energy count
+            int energyCount = count(getDeck(), new Energy());
+            //make a new deck for next run of trials
             makeDeck(pokemonCount, energyCount, candyCount);
         }
     }
 
+    /**
+     * Determines if the Prize Cards contain a Rare Candy card.
+     * @param handEvaluationResult the result of seeing a Rare Candy in the hand.
+     * @return True if a Rare Candy exists in the Prizes without being in the hand.
+     * False otherwise.
+     */
     public boolean evaluatePrizes(boolean handEvaluationResult) {
         //if the hand does not have a rare candy
         if (!handEvaluationResult) {
@@ -110,6 +125,11 @@ public class RareCandyHands extends PokemonHands{
         return false;
     }
 
+    /**
+     * Determines if the hand contains both a Pokemon and a Rare Candy.
+     * @return The logical AND of two variables representing truth values
+     * for seeing a Pokemon in the hand and a Rare Candy in the hand.
+     */
     @Override
     public boolean evaluateHand() {
         boolean hasPokemon = false;
@@ -129,6 +149,13 @@ public class RareCandyHands extends PokemonHands{
         return hasPokemon && hasCandy;
     }
 
+    /**
+     * Populates a Stack Data Structure with passed amounts of card objects, and fills the
+     * rest of the space with Energy cards. Then, sets the Stack as the global Deck object.
+     * @param pokemonCount number of Pokemon to add to the deck
+     * @param energyCount number of Energy to add to the deck
+     * @param candyCount number of Rare Candy to add to the deck
+     */
     public void makeDeck(int pokemonCount, int energyCount, int candyCount) {
         //make a new Deck Object
         Stack<PokemonCard> newDeck = new Stack<>();
@@ -156,8 +183,7 @@ public class RareCandyHands extends PokemonHands{
     }
 
     /**
-     * After making the Deck data structure, populates the Prizes
-     * array with six cards drawn from the top of the deck.
+     * Populates the Prizes array with six cards drawn from the top of the deck.
      */
     public void dealPrizes(){
         //shuffle the deck
@@ -176,14 +202,63 @@ public class RareCandyHands extends PokemonHands{
         setDeck(theDeck);
     }
 
+    @Override
+    public String exportObjects(String filePath) throws IOException {
+        FileWriter fr = new FileWriter(filePath);
+        BufferedWriter bfr = new BufferedWriter(fr);
+        //header
+        bfr.write("Pokemon in Deck,Rare Candy in Deck,Rate of Success,Rate of Bricks");
+        bfr.newLine();
+        //data
+        for(CandyHandResult h : getCandyResults()){
+            bfr.write(h.getNumPokemon() + "," + h.getNumCandy() + "," +
+                    h.getSuccessRate() + "," + h.getPrizeCandyRate());
+            bfr.newLine();
+        }
+        //close writers
+        bfr.close();
+        fr.close();
+        //inform user of success
+        return "File successfully created at " + filePath;
+    }
+
+    /**
+     * Creates a deck with 16 copies of Pokemon and Energy, and one Rare Candy.
+     * Fills the rest of the deck with energy afterward. Initializes all other
+     * data structures for use in program.
+     */
+    public RareCandyHands(){
+        //make a deck with 16 pokemon, 16 energy, and 1 Rare Candy
+        makeDeck(16, 16, 1);
+        //set hand size to 7
+        setHandSize(7);
+        //set the hand to an empty arraylist
+        setTheHand(new ArrayList<>());
+        //set the results to an empty arraylist
+        setCandyResults(new ArrayList<>());
+        //set prizes as new array
+        setPrizes(new PokemonCard[getPrizeCount()]);
+    }
+
+    /**
+     * Parameterized constructor allows user to specify deck contents and hand size.
+     * Also initializes all other data structures for use in program.
+     * @param aDeck a premade deck list. if not populated, uses passed values to make deck
+     * @param pokemonCount number of Pokemon to include
+     * @param energyCount number of energy to include
+     * @param candyCount number of Rare Candy to include
+     * @param handSize the desired hand size
+     */
     public RareCandyHands(Stack<PokemonCard> aDeck, int pokemonCount, int energyCount,
                           int candyCount, int handSize){
-        //if the passed deck is not null,
+        //if the passed deck is not null and is the correct size,
         if(aDeck != null){
-            //set it as the deck
-            setDeck(aDeck);
+            if(aDeck.size() == 60) {
+                //set it as the deck
+                setDeck(aDeck);
+            }
         //otherwise
-        }else {
+        }else{
             //make a deck data structure
             makeDeck(pokemonCount, energyCount, candyCount);
         }
@@ -192,7 +267,7 @@ public class RareCandyHands extends PokemonHands{
         //set hand as new arraylist
         setTheHand(new ArrayList<>());
         //set the results to an empty arraylist
-        setTheResults(new ArrayList<>());
+        setCandyResults(new ArrayList<>());
         //set prizes as new array
         setPrizes(new PokemonCard[getPrizeCount()]);
     }
@@ -208,6 +283,16 @@ public class RareCandyHands extends PokemonHands{
     public int getPrizeCount(){
         return PRIZE_COUNT;
     }
+
+    public ArrayList<CandyHandResult> getCandyResults(){
+        return theResults;
+    }
+
+    public void setCandyResults(ArrayList<CandyHandResult> theResults){
+        this.theResults = theResults;
+    }
+
+    private ArrayList<CandyHandResult> theResults;
 
     private PokemonCard[] prizes;
 
