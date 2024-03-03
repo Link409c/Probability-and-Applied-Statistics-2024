d5055daa5757e793b2674e1a874561b9bce949f8
CSimpson <cdsimpsonnavy2010@yahoo.com>
2024-03-03 02:39:24 -0500

    added brick rate variable.

--- src/MiniProjects/PokemonProject/PokemonCardHands/Structures/CandyHandResult.java
+++ src/MiniProjects/PokemonProject/PokemonCardHands/Structures/CandyHandResult.java
@@ -2,9 +2,10 @@ package MiniProjects.PokemonProject.PokemonCardHands.Structures;
 
 public class CandyHandResult extends HandResult{
 
-    public CandyHandResult(int numPokemon, double successRate, int numCandy){
-        super(numPokemon, successRate);
+    public CandyHandResult(int numPokemon, double handCandy, int numCandy, double prizeCandy){
+        super(numPokemon, handCandy);
         setNumCandy(numCandy);
+        setPrizeCandyRate(prizeCandy);
     }
     public int getNumCandy() {
         return numCandy;
@@ -14,12 +15,21 @@ public class CandyHandResult extends HandResult{
         this.numCandy = numCandy;
     }
 
+    public double getPrizeCandyRate() {
+        return prizeCandyRate;
+    }
+
+    public void setPrizeCandyRate(double prizeCandyRate) {
+        this.prizeCandyRate = prizeCandyRate;
+    }
+
     /**
      * the total number of "Rare Candy" cards in the deck.
      */
     private int numCandy;
-
-    private boolean handHasCandy;
-
-    private boolean prizesHasCandy;
+    /**
+     * the probability of seeing a rare candy
+     * in the prizes for this set of trials.
+     */
+    private double prizeCandyRate;
 }
