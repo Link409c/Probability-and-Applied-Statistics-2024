b0518a9e83d4866791a44749920f2085f5ab5d44
link409c <cdsimpsonnavy2010@yahoo.com>
2024-03-08 00:20:46 -0500

    programming build game method and conduct turn method.

--- src/MiniProjects/PokemonProject/PokemonTCG/Program/PokemonGame.java
+++ src/MiniProjects/PokemonProject/PokemonTCG/Program/PokemonGame.java
@@ -12,8 +12,19 @@ import java.util.Collections;
 import java.util.Random;
 import java.util.Scanner;
 
+/**
+ * Pokemon Game is a program designed to emulate simple functions of the Pokemon Trading Card Game.
+ * Two players can participate to play the game against one another, winning when their opponent has
+ * no available Pokemon to play, or they take all Prizes from their Prize Pile. Players can import their
+ * own custom deck lists, or participate in a game with premade deck lists.
+ */
 public class PokemonGame implements CardGameEnvironment<PokemonCard, PokemonPlayer>, FileAble {
 
+    /**
+     * instantiates the player objects, builds the game environment, and runs the game
+     * until a player is declared the winner.
+     * @return the winner of the game.
+     */
     @Override
     public PokemonPlayer runGame() {
         //build necessary game objects
@@ -39,45 +50,104 @@ public class PokemonGame implements CardGameEnvironment<PokemonCard, PokemonPlay
         }
     }
 
+    /**
+     * draws hands for both players, and prompts the players to set their active
+     * pokemon and benched pokemon. then, sets each player's prize piles.
+     */
     @Override
     public void buildGame() {
-        //check hands and mulligan while player has no pokemon
         PokemonPlayer currPlayer;
-        for(int i = 0; i < 2; i++) {
-            if(i == 0){
+        Scanner in = new Scanner(System.in);
+        //check hands and mulligan while player has no pokemon
+        //then choose active and benched pokemon
+        for (int i = 0; i < 2; i++) {
+            //list for valid targets of search
+            ArrayList<PokemonCard> pokeList = new ArrayList<>();
+            //TODO: wrap the loop contents in a method
+            if (i == 0) {
                 currPlayer = getPlayerOne();
-                while(!currPlayer.getHand().contains(Pokemon.class)) {
+                while (!currPlayer.getHand().contains(Pokemon.class)) {
+                    //if curr player mulligans, opponent gets to draw a card
                     currPlayer.mulliganHand();
                     getPlayerTwo().getHand().add(getPlayerTwo().getDeck().pop());
                 }
+                System.out.println("Choose your active Pokemon, " + currPlayer);
+                System.out.println("Place some Pokemon on the bench?");
+                //if no
+                System.out.println("");
                 setPlayerOne(currPlayer);
-            }else{
+            } else {
                 currPlayer = getPlayerTwo();
-                while(!currPlayer.getHand().contains(Pokemon.class)) {
+                while (!currPlayer.getHand().contains(Pokemon.class)) {
+                    //if curr player mulligans, opponent gets to draw a card
                     currPlayer.mulliganHand();
                     getPlayerOne().getHand().add(getPlayerOne().getDeck().pop());
                 }
-                setPlayerTwo(currPlayer);
+                System.out.println("Choose your active Pokemon, " + currPlayer);
+                //print the list of pokemon to choose
+                //TODO: make displayHand method to replace this
+                for (PokemonCard c : getPlayerTwo().getHand()) {
+                    if (c.getClass() == Pokemon.class) {
+                        pokeList.add(c);
+                    }
+                }
+                System.out.println("Valid Targets:");
+                for (PokemonCard c : pokeList) {
+                    System.out.println(c.getCardName());
+                }
+                //get input
+                String choice = in.next();
+                //remove the pokemon placed from the hand
+                //TODO: Make a method in player to do this
+                for (int j = 0; j < currPlayer.getHand().size(); j++) {
+                    PokemonCard c = getPlayerTwo().getHand().get(j);
+                    if (choice.equalsIgnoreCase(c.getCardName())) {
+                        currPlayer.setActivePokemon((Pokemon) pokeList.remove(j));
+                        currPlayer.getHand().remove(c);
+                    }
+                }
+                //print list to put on bench
+                System.out.println("Add Pokemon to Bench:");
+                System.out.println("Valid Targets:");
+                for (PokemonCard c : pokeList) {
+                    System.out.println(c.getCardName());
+                }
+                while (currPlayer.hasPokemon()) {
+                    choice = in.next();
+                    for (int j = 0; j < currPlayer.getHand().size(); j++) {
+                        PokemonCard c = getPlayerTwo().getHand().get(j);
+                        if (choice.equalsIgnoreCase(c.getCardName())) {
+                            currPlayer.setActivePokemon((Pokemon) pokeList.remove(j));
+                            currPlayer.getHand().remove(c);
+                        }
+                    }
+                    //if no, continue
+                    //set bench
+                }
             }
+            //after active pokemon and benches are set, set each player's prize cards
+            setPlayerTwo(currPlayer);
         }
-        Scanner in = new Scanner(System.in);
-        //players choose their active pokemon and pokemon to add to the bench
-        System.out.println("Choose your active Pokemon, " + getPlayerOne());
-        //after active pokemon and benches are set, set each player's prize cards
-        //choose who goes first
     }
 
+
+    /**
+     * Conducts a player's turn. This consists of drawing a card, playing any number of
+     * trainers, supporters, and items, and attacking with the active pokemon.
+     * @param turnPlayer the player whose turn it is
+     * @return the turn player object
+     */
     @Override
     public PokemonPlayer conductTurn(PokemonPlayer turnPlayer) {
         //the turn player
         //draws a card
         turnPlayer.drawCard();
-        //plays items, trainers, supporters, stadiums, attaches energy, evolves pokemon.
         boolean declaredAttack = false;
         while(!declaredAttack){
-            //do stuff
+            //plays items, trainers, supporters, stadiums, attaches energy, evolves pokemon.
+            //get user input for what to do
+            //if attack, set flag to true
             //attack using the active pokemon.
-            //if you attack, set the flag to true
         }
         //after attacking the turn ends and the other player takes their turn.
         return turnPlayer;
@@ -172,11 +242,19 @@ public class PokemonGame implements CardGameEnvironment<PokemonCard, PokemonPlay
         return null;
     }
 
+    /**
+     * Parameterized constructor sets player objects to passed parameters.
+     * @param playerA Player One
+     * @param playerB Player Two
+     */
     public PokemonGame(PokemonPlayer playerA, PokemonPlayer playerB){
         setPlayerOne(playerA);
         setPlayerTwo(playerB);
     }
 
+    /**
+     * Default constructor initializes new player objects.
+     */
     public PokemonGame() {
         setPlayerOne(new PokemonPlayer());
         setPlayerTwo(new PokemonPlayer());
@@ -198,6 +276,13 @@ public class PokemonGame implements CardGameEnvironment<PokemonCard, PokemonPlay
         this.playerTwo = playerTwo;
     }
 
+    /**
+     * the object representing Player One.
+     */
     private PokemonPlayer playerOne;
+
+    /**
+     * the object representing Player Two.
+     */
     private PokemonPlayer playerTwo;
 }
