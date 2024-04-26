133e7d187eddf11f7bcc0389c33ca0adb648b4eb
CSimpson <cdsimpsonnavy2010@yahoo.com>
2024-04-26 18:13:33 -0400

    added pseudocode for run program method.

--- src/main/java/Project2/PlotSaltSmooth/Programs/ExternalAPIPrograms/NewSalter.java
+++ src/main/java/Project2/PlotSaltSmooth/Programs/ExternalAPIPrograms/NewSalter.java
@@ -1,6 +1,28 @@
 package Project2.PlotSaltSmooth.Programs.ExternalAPIPrograms;
 
 import Project2.PlotSaltSmooth.Programs.UserDefinedPrograms.Salter;
+import Project2.PlotSaltSmooth.Structures.Tuple;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class NewSalter extends Salter {
+
+    public void runProgram(String filePath, ArrayList<Tuple<Double>> plottedPoints) throws IOException {
+        //if plottedPoints is null,
+        if(plottedPoints == null) {
+            //if filePath is null or invalid,
+            if (filePath == null || filePath.isEmpty()) {
+                //throw an exception
+                throw new IllegalArgumentException("Passed data to the salter is not valid.");
+                //else import the data from a file.
+            } else {
+                importObjects(filePath);
+            }
+        }
+        //salt
+        for(Tuple<Double> t : getSaltedPoints()){
+            //call salting function from apache here for each output value
+        }
+    }
 }
--- src/main/java/Project2/PlotSaltSmooth/Programs/ExternalAPIPrograms/NewSmoother.java
+++ src/main/java/Project2/PlotSaltSmooth/Programs/ExternalAPIPrograms/NewSmoother.java
@@ -1,6 +1,27 @@
 package Project2.PlotSaltSmooth.Programs.ExternalAPIPrograms;
 
 import Project2.PlotSaltSmooth.Programs.UserDefinedPrograms.Smoother;
+import Project2.PlotSaltSmooth.Structures.Tuple;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class NewSmoother extends Smoother {
+    public void runProgram(String filePath, ArrayList<Tuple<Double>> plottedPoints) throws IOException {
+        //if plottedPoints is null,
+        if(plottedPoints == null) {
+            //if filePath is null or invalid,
+            if (filePath == null || filePath.isEmpty()) {
+                //throw an exception
+                throw new IllegalArgumentException("Passed data to the salter is not valid.");
+                //else import the data from a file.
+            } else {
+                importObjects(filePath);
+            }
+        }
+        //smooth the data
+        for(Tuple<Double> t : getSmoothedPoints()){
+            //call smoothing function from apache here for each output value
+        }
+    }
 }
